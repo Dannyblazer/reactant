988160a67f9f3547644aa743d449da0972e1377b
Dannyblazer <nnajidanny004@gmail.com>
2023-04-25 13:27:45 +0100

    initial commit

---
+++ src/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+    return (
+        <div className="movie">
+            <div>
+                <p></p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
